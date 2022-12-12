c3f71fa3b1bd6458cf79826ad30ce561dba54a6b
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-12 13:29:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import pandas as pd
 import streamlit as st
 import joblib
 
-"""
-# Welcome to Bandora!
-## A peer lending firm
 
-"""
+
+st.title('Welcome to Bandora')
+st.header('A peer to peer lending firm')
+
