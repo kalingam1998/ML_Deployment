e0d4fc7280a8dd3c6615777827cc896a1a713987
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-12 13:50:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ import joblib
 st.title('Welcome to Bandora')
 st.header('A peer to peer lending firm')
 
+# following lines create boxes in which user can enter data required to make prediction 
+Language = st.selectbox('Language',("Estonian","English", "Russian","Finnish", "German","Spanish","Slovakian"))
+LoanDuration = st.number_input('Loan Duration (in months)') 
+HomeOwnershipType = st.selectbox('Home Ownership Type',("Homeless","Owner","Living with parents","Tenant","pre-furnished property","Tenant", 
+                                                        "unfurnished property","Council house","Joint tenant","Joint ownership","Mortgage",
+                                                        "Owner with encumbrance","Other"))
+Restructured = st.selectbox('Restructured',("Yes","No"))
