36c6e73f312fdd53620ccb0c246ed9781b4d3167
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-13 17:18:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,42 @@
 import pandas as pd
 import streamlit as st
 import joblib
+classifier_pipeline = joblib.load('RFC_pipeline_FINAL.joblib')
 
+def create_input_Dataframe():
+  
+  input_dictionary = {
+    "Language" : Language,
+    "HomeOwnershipType": HomeOwnershipType,
+    "Restructured" : Restructured,
+    "IncomeTotal" : IncomeTotal,
+    "LiabilitiesTotal" : LiabilitiesTotal,
+    "LoanDuration" : LoanDuration,
+    "EMI": EMI,
+    "PreviousRepaymentsBeforeLoan" : PreviousRepaymentsBeforeLoan,
+    "MonthlyPaymentDay" :MonthlyPaymentDay,
+    
+    "PrincipalPaymentsMade" : PrincipalPaymentsMade,
+    "InterestAndPenaltyPaymentsMade" : InterestAndPenaltyPaymentsMade,
+    "PrincipalBalance" : PrincipalBalance,
+    "InterestAndPenaltyBalance" : InterestAndPenaltyBalance,
+    "Bids" : BidsPortfolioManger+BidsApi,
+    "Rating" : Rating
+  }
+  
+  DF = pd.DataFrame(input_dictionary)
+  
+  return DF
 
+def Classifier():
+  input = create_input_Dataframe(index=[0])
+  prediction = classifier_pipeline.predict(input)
+  if prediction==1:
+    result = "Defaulter"
+  if prediction==0:
+    result = "Not Defaulter"
+  
+  return result
 
 st.title('Bandora Loan Approval Dashboard')
 
@@ -14,6 +48,7 @@ HomeOwnershipType = st.selectbox('Home Ownership Type',("Homeless","Owner","Livi
                                                         "unfurnished property","Council house","Joint tenant","Joint ownership","Mortgage",
                                                         "Owner with encumbrance","Other"))
 Restructured = st.selectbox('Restructured',("Yes","No"))
+IncomeTotal = st.text_input('Total Icome')
 LiabilitiesTotal = st.text_input('Total Liabilities')
 
 st.subheader('Loan Details')
@@ -22,11 +57,32 @@ AppliedAmount = st.text_input('Applied Loan Amount')
 EMI = st.text_input('Equated Monthly Installment')
 
 st.subheader('Payment Details')
+PreviousRepaymentsBeforeLoan = st.text_input('PreviousRepaymentsBeforeLoan')
+MonthlyPaymentDay = st.text_input('MonthlyPaymentDay (digit)')
 PrincipalPaymentsMade = st.text_input('Principal Payments Made') 
 InterestAndPenaltyPaymentsMade = st.text_input('Interest and Penalty Payments Made')
+
+st.subheader('Balance Details')
+PrincipalBalance = st.text_input('PrincipalBalance')
+InterestAndPenaltyBalance = st.text_input('InterestAndPenaltyBalance')
+
+st.subheader('Amount of Investment offers made via')
+BidsPortfolioManger = st.text_input('BidsPortfolioManger')
+BidsApi = st.text_input('BidsApi')
 
 st.subheader('Other')
 Rating = st.selectbox('Rating',("a","aa", "b","c", "d","e","f","hr"))
 
+st.header('Loan Application Status')
+if st.form_submit_button(label="Check Status"):
+  result = Classifier()
+  st.spinner(text="In process...")
+  
+  if result=="Defaulter":
+    st.write("Rejected")
+  
+  if result=="Not Defaulter":
+    st.write("Approved")
+  
 
 
