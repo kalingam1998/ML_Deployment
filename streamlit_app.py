500864f0a4f5a3e6db29a21b0c844590f8b464ae
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-13 15:37:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,29 @@ import joblib
 
 
 
-st.title('Welcome to Bandora')
-st.header('A peer to peer lending firm')
+st.title('Bandora Loan Approval Dashboard')
 
-# following lines create boxes in which user can enter data required to make prediction 
+st.header("Borrower's Information")
+
+st.subheader('Personal Background')
 Language = st.selectbox('Language',("Estonian","English", "Russian","Finnish", "German","Spanish","Slovakian"))
-LoanDuration = st.number_input('Loan Duration (in months)') 
 HomeOwnershipType = st.selectbox('Home Ownership Type',("Homeless","Owner","Living with parents","Tenant","pre-furnished property","Tenant", 
                                                         "unfurnished property","Council house","Joint tenant","Joint ownership","Mortgage",
                                                         "Owner with encumbrance","Other"))
 Restructured = st.selectbox('Restructured',("Yes","No"))
+LiabilitiesTotal = st.number_input('Total Liabilities')
+
+st.subheader('Loan Details')
+LoanDuration = st.number_input('Loan Duration (in months)') 
+AppliedAmount = st.number_input('Applied Loan Amount')
+EMI = st.number_input('Equated Monthly Installment')
+
+st.subheader('Payment Details')
+PrincipalPaymentsMade = st.number_input('Principal Payments Made') 
+InterestAndPenaltyPaymentsMade = st.number_input('Interest and Penalty Payments Made')
+
+st.subheader('Other')
+Rating = st.selectbox('Rating',("a","aa", "b","c", "d","e","f","hr"))
+
+
+
