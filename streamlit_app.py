c7e40c54ffff0e4ea19af51d75db8335526a26f6
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-13 15:42:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,13 @@ Restructured = st.selectbox('Restructured',("Yes","No"))
 LiabilitiesTotal = st.text_input('Total Liabilities')
 
 st.subheader('Loan Details')
-LoanDuration = st.number_input('Loan Duration (in months)') 
-AppliedAmount = st.number_input('Applied Loan Amount')
-EMI = st.number_input('Equated Monthly Installment')
+LoanDuration = st.text_input('Loan Duration (in months)') 
+AppliedAmount = st.text_input('Applied Loan Amount')
+EMI = st.text_input('Equated Monthly Installment')
 
 st.subheader('Payment Details')
-PrincipalPaymentsMade = st.number_input('Principal Payments Made') 
-InterestAndPenaltyPaymentsMade = st.number_input('Interest and Penalty Payments Made')
+PrincipalPaymentsMade = st.text_input('Principal Payments Made') 
+InterestAndPenaltyPaymentsMade = st.text_input('Interest and Penalty Payments Made')
 
 st.subheader('Other')
 Rating = st.selectbox('Rating',("a","aa", "b","c", "d","e","f","hr"))
